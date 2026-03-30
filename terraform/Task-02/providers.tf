1b1aae867695dc943687ddb2087de8d97f6b84b5
Ajay Kumar <68456769+AjayKumar-91@users.noreply.github.com>
2026-03-30 17:12:17 +0530

    Create providers.tf

---
+++ terraform/Task-02/providers.tf
@@ -0,0 +1,11 @@
+# Mumbai Region Provider
+provider "aws" {
+  alias  = "mumbai"
+  region = "ap-south-1"
+}
+
+# Virginia Region Provider
+provider "aws" {
+  alias  = "virginia"
+  region = "us-east-1"
+}
